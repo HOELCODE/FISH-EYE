ff2c32aded979861fb660ca88f60b8143234e7b1
HOELCODE <hcostantini1@gmail.com>
2025-01-24 08:32:24 +0100

    first commit

---
+++ scripts/pages/homePage.js
@@ -0,0 +1,33 @@
+    // fonction pour récupérer les datas
+    async function getPhotographers() {
+        try {
+            const response = await fetch('data/photographers.json');
+            if (!response.ok) {
+                throw new Error("Pas de réponse");
+            }
+            return await response.json();
+
+        } catch (error) {
+            console.error("Erreur lors du fetch", error);
+        }
+    }
+    
+    // Fonction pour récupérer le template de display et associer les données récupérées
+    async function displayData(photographers) {
+        const photographersSection = document.querySelector(".photographer_section");
+
+        photographers.forEach((photographer) => {
+            const photographerModel = photographerTemplate(photographer);
+            const userCardDOM = photographerModel.getUserCardDOM();
+            photographersSection.appendChild(userCardDOM);
+        });
+    }
+    
+    //Fonction pour lancer le fetch et display les datas 
+    async function init() {
+        // Récupère les datas des photographes
+        const { photographers } = await getPhotographers();
+        displayData(photographers);
+    }
+    
+    init();
---
+++ scripts/templates/homePage.js
@@ -0,0 +1,29 @@
+function photographerTemplate(data) {
+    const { name, portrait, city, country, tagline, price, id } = data;
+
+    const picture = `assets/photographes/photos-photographes/${portrait}`;
+
+    function getUserCardDOM() {
+        //Créer les éléments
+        const article = document.createElement( 'article' );
+
+        //Photographer card 
+        const photogrpaherCard = `
+            <a href="photographer.html?id=${id}"> 
+                <img src="${picture}" alt="photo de ${name}">
+                <h2>${name}</h2>
+            </a>
+            <div>
+                <h3>${city}, ${country} </h3>
+                <p>${tagline}</p>
+                <span>${price}€/jour</span>
+            </div>
+        `
+
+        //Associer Photographer card à article
+        article.innerHTML = photogrpaherCard;
+
+        return (article);
+    }
+    return { name, picture, getUserCardDOM }
+}
