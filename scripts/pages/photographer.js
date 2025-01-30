ebc2e6c71e095f9285fef61e29bb061d328ec4ba
HOELCODE <hcostantini1@gmail.com>
2025-01-30 12:54:45 +0100

    home ok, photographe 30%

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,68 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// fonction pour récupérer les datas
+async function getPhotographers() {
+    try {
+        const response = await fetch('data/photographers.json');
+        if (!response.ok) {
+            throw new Error("Pas de réponse");
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("Erreur lors du fetch", error);
+    }
+}
+
+// fonction pour récupérer uniquement les medias
+async function getMedia() {
+    try {
+        const response = await fetch('data/photographers.json');
+        if (!response.ok) {
+            throw new Error("Pas de réponse");
+        }
+        const data = await response.json();
+        return data.media;  // Retourner uniquement la partie media
+    } catch (error) {
+        console.error("Erreur lors du fetch", error);
+    }
+}
+
+//Fonction pour associer la bonne galerie en fonction de l'id du photographe
+async function getGalerie() {
+    const media = await getMedia();
+    const { photographers } = await getPhotographers();
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
+
+
+    media.forEach(element => {
+        if (element.photographerId === parseInt(id)) {
+            var lid = element.photographerId;
+            for (i = 0; i < photographers.length; i++) {
+                if (photographers[i].id === lid && element.hasOwnProperty('image') && element.image !== undefined) {
+                    createDomGalerie(element, photographers[i].name)
+                }
+            }
+        }
+    });
+
+}
+
+// fonction pour associer le photographe en fonction de l'id dans l'url 
+async function getUrl() {
+    const { photographers } = await getPhotographers();
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
+
+    photographers.forEach(element => {
+        if (element.id === parseInt(id)) {
+            createDomHeader(element);
+        }
+    });
+}
+
+//fonction pour lancer les différentes fonctions
+async function init() {
+    await getUrl();  // Assure que les fonctions asynchrones sont bien exécutées
+    await getGalerie();
+}
+
+init();
---
+++ scripts/templates/photographerTemplate.js
@@ -0,0 +1,48 @@
+//fonction pour afficher les éléments dans le dom 
+async function createDomHeader(photographe) {
+    //Récupérer les éléments du DOM
+    const headerContainer = document.querySelector(".photograph-header");
+
+    //Créer le bloque html 
+    const photographerHeader = `
+        <div class="photograph-description">
+            <h1 class="photograph-name">${photographe.name}</h1>
+            <p class="photograph-localisation">${photographe.city}, ${photographe.country}</p>
+            <span class="photograph-slogan">${photographe.tagline}</span>
+        </div>
+        <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
+        <img class="image-photographe" src="assets/photographes/photos-photographes/${photographe.portrait}" alt="photo de ${photographe.name}">
+    `
+    
+    //Associer le header
+    headerContainer.innerHTML = photographerHeader;
+
+    return (headerContainer);
+}
+
+// Fonction pour afficher la galerie
+async function createDomGalerie(collection, artiste) {
+    // Récupérer les éléments du DOM
+    const sectionGalerie = document.querySelector(".galerie");
+
+    // Créer le bloc HTML
+    const photographeGalerie = `
+        <article class="galerie-article">
+          <img class="img-photo" src="assets/photographes/${artiste}/${collection.image}" alt="${collection.title}">
+          <div class="div-description">
+            <p class="galerie-description">${collection.title}</p>
+            <div class="galerie-likes">
+              <span class="galerie-like">${collection.likes}</span>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+          </div>
+        </article>
+    `;
+
+    // Ajouter l'article à la galerie sans remplacer le contenu existant
+    sectionGalerie.insertAdjacentHTML('beforeend', photographeGalerie);
+}
+
+
+
+
