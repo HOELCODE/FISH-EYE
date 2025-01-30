98a12d938127d0850501d0d6d09f26a4014aec9d
HOELCODE <hcostantini1@gmail.com>
2025-01-30 16:59:01 +0100

    affichage form

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,10 @@
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
+    const modal = document.getElementById("contact-modal");
 	modal.style.display = "block";
+    modal.style.marginTop = "-118%";
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
+    const modal = document.getElementById("contact-modal");
     modal.style.display = "none";
 }
